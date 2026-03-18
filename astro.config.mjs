a7e4eda13ab1104fd14d8c9901bb54dfa3eb146d
letinhclover <57601291+letinhclover@users.noreply.github.com>
2026-03-18 08:12:35 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import react    from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
@@ -6,6 +7,7 @@ export default defineConfig({
   output: 'static',
 
   integrations: [
+    react(),
     tailwind({
       applyBaseStyles: false,
       configFile: './tailwind.config.mjs',
